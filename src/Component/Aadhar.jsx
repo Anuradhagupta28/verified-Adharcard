bde4aeb78b4262505f9df116ac3a0d84c8c67c7c
Anuradhagupta28 <annugupta8512@gmail.com>
2024-01-12 22:55:48 +0530

    update params

--- src/Component/Aadhar.jsx
+++ src/Component/Aadhar.jsx
@@ -1,14 +1,10 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
-
-import Aadhar from './Component/Aadhar';
+import Loader from './Loader';
 import { useLocation, useParams } from 'react-router-dom';
-import Loader from './Component/Loader';
-function App() {
 
+function Aadhar() {
   const [loading, setLoading] = useState(true);
-  const [code, setCode] = useState(null);
-
+  const { code } = useParams();
 
   useEffect(() => {
   //   const fetchData = async () => {
@@ -17,21 +13,11 @@ function App() {
   //     setLoading(false);
   //   };
   //  fetchData();
-   
+    console.log('Code:', code);
+  }, [code]);
 
-  const getQueryParam = (name) => {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
-  };
-  const codeFromURL = getQueryParam('code');
-
-    // Set the 'code' state
-    setCode(codeFromURL);
-  }, []);
   return (
-
-    <div className="App">
-     {/* <div >
+    <div >
       {loading ? (
         // Show loader while loading is true
         <div >
@@ -47,18 +33,8 @@ function App() {
       )}
      <p>Extracted Code: {code}</p>
 
-    </div> */}
-
-<div>
-      {code ? (
-        <p>Code: {code}</p>
-      ) : (
-        <p>No code parameter found in the URL.</p>
-      )}
-    </div>
     </div>
   );
 }
 
-export default App;
-
+export default Aadhar;
