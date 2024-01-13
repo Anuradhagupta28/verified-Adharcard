018d517c6e319ef1f1f91a83fc689951ed420688
Anuradhagupta28 <annugupta8512@gmail.com>
2024-01-13 23:04:40 +0530

    update params

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-import Aadhar from './Component/Aadhar';
 import { useLocation, useParams } from 'react-router-dom';
 import Loader from './Component/Loader';
 function App() {
 
   const [loading, setLoading] = useState(true);
   const [code, setCode] = useState(null);
+  const [token, setToken] = useState('');
+
+  
 
 
   useEffect(() => {
@@ -24,10 +26,62 @@ function App() {
     return urlParams.get(name);
   };
   const codeFromURL = getQueryParam('code');
-
-    // Set the 'code' state
     setCode(codeFromURL);
+
+   
+  //   console.log("data")
+   
+    const getToken = async () => {
+      const url = 'https://digilocker.meripehchaan.gov.in/public/oauth2/2/token';
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+      // const Code = '049d89292e9ec291c5b4b390981f81597967233f';
+      const grantType = 'authorization_code';
+      const clientId = 'AW6A72E90E';
+      const clientSecret = '88c23091c8b73a89b7a2';
+      const redirectUri = 'https://verified-adharcard.vercel.app/';
+      const codeVerifier = '_34W.h85p9~C_E5afi.ALG.yV_dD2Adq~Xda4k3W7U8n8y';
+
+  //     const clientId = 'free_tier_annugupta8512_3efccbeb9f'; // Replace with your actual client ID
+  // const clientSecret = '03a7294e16e54c548c694fe678d2ede1'; // Replace with your actual client secret
+    
+      // const bodyData = `code=${encodeURIComponent(Code)}&grant_type=${encodeURIComponent(grantType)}&client_id=${encodeURIComponent(clientId)}&client_secret=${encodeURIComponent(clientSecret)}&redirect_uri=${encodeURIComponent(redirectUri)}&code_verifier=${encodeURIComponent(codeVerifier)}`;
+     const bodyData = `code=${encodeURIComponent(code)}&grant_type=${encodeURIComponent(grantType)}&client_id=${encodeURIComponent(clientId)}&client_secret=${encodeURIComponent(clientSecret)}&redirect_uri=${encodeURIComponent(redirectUri)}&code_verifier=${encodeURIComponent(codeVerifier)}`;
+  
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: headers,
+          body: bodyData,
+          mode: 'no-cors',
+        });
+    
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+    
+        const data = await response.json();
+        console.log('data response', data);
+        console.log(data.access_token);
+        setToken(data.access_token);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        console.log(error);
+      }
+    };
+    
+    // Call the async function
+    getToken();
+    
+
+
+    
   }, []);
+
+
+
+  // 
   return (
 
     <div className="App">
@@ -51,10 +105,14 @@ function App() {
 
 <div>
       {code ? (
-        <p>Code: {code}</p>
+       <div>
+         <p>Code: {code}</p>
+         <h3>Acces token:{token}</h3>
+        </div>
       ) : (
         <p>No code parameter found in the URL.</p>
       )}
+       
     </div>
     </div>
   );
--- src/Component/Aadhar.jsx
+++ src/Component/Aadhar.jsx
@@ -1,40 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import Loader from './Loader';
-import { useLocation, useParams } from 'react-router-dom';
+// import React, { useState, useEffect } from 'react';
+// import Loader from './Loader';
+// import { useLocation, useParams } from 'react-router-dom';
 
-function Aadhar() {
-  const [loading, setLoading] = useState(true);
-  const { code } = useParams();
+// function Aadhar() {
+//   const [loading, setLoading] = useState(true);
+//   const { code } = useParams();
 
-  useEffect(() => {
-  //   const fetchData = async () => {
-  //     await new Promise(resolve => setTimeout(resolve, 2000));
+//   useEffect(() => {
+//   //   const fetchData = async () => {
+//   //     await new Promise(resolve => setTimeout(resolve, 2000));
 
-  //     setLoading(false);
-  //   };
-  //  fetchData();
-    console.log('Code:', code);
-  }, [code]);
+//   //     setLoading(false);
+//   //   };
+//   //  fetchData();
 
-  return (
-    <div >
-      {loading ? (
-        // Show loader while loading is true
-        <div >
-            <Loader/>
-        </div>
-      ) : (
-        // Show image when loading is false
-        <img
-          className="responsiveImage"
-          src="https://media.giphy.com/media/PijzuUzUhm7hcWinGn/giphy.gif"
-          alt="verifiedImage"
-        />
-      )}
-     <p>Extracted Code: {code}</p>
+//   const handleTokenRequest = async () => {
+//     const apiUrl = 'https://digilocker.meripehchaan.gov.in/public/oauth2/2/token';
 
-    </div>
-  );
-}
+//     const requestBody = new URLSearchParams();
+//     requestBody.append('code', 'your_code_value');
+//     requestBody.append('grant_type', 'authorization_code');
+//     requestBody.append('client_id', 'your_client_id');
+//     requestBody.append('client_secret', 'your_client_secret');
+//     requestBody.append('redirect_uri', 'your_redirect_uri');
+//     requestBody.append('code_verifier', 'your_code_verifier');
 
-export default Aadhar;
+//     try {
+//       const response = await fetch(apiUrl, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//         body: requestBody,
+//       });
+
+//       const responseData = await response.json();
+//       setToken(responseData.access_token);
+
+//       // Handle other response data as needed
+//     } catch (error) {
+//       console.error('Error fetching token:', error);
+//     }
+//   };
+//     console.log('Code:', code);
+//   }, [code]);
+
+//   return (
+//     <div >
+//       {loading ? (
+//         // Show loader while loading is true
+//         <div >
+//             <Loader/>
+//         </div>
+//       ) : (
+//         // Show image when loading is false
+//         <img
+//           className="responsiveImage"
+//           src="https://media.giphy.com/media/PijzuUzUhm7hcWinGn/giphy.gif"
+//           alt="verifiedImage"
+//         />
+//       )}
+//      <p>Extracted Code: {code}</p>
+
+//     </div>
+//   );
+// }
+
+// export default Aadhar;
